986c2f06670c4e74bc072bcddc774b142b0c4589
Gao Chengxin <gaochengxin@cestc.cn>
2022-01-23 19:42:20 +0800

    添加项目文件。

---
+++ UnderstandingUnitProgramming.cpp
@@ -0,0 +1,12 @@
+﻿// UnderstandingUnitProgramming.cpp: 定义应用程序的入口点。
+//
+
+#include "UnderstandingUnitProgramming.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
